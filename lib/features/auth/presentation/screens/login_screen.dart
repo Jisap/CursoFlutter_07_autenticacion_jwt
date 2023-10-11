2bbec672802d2506332a77c95210b99da7813ef3
Jorge Matos <jisapjisap@gmail.com>
2023-10-11 17:47:19 +0200

    Mostrar el error

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:teslo_shop/features/auth/providers/auth_provider.dart';
 import 'package:teslo_shop/features/auth/providers/login_form_provider.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
@@ -51,12 +52,25 @@ class LoginScreen extends StatelessWidget {
 }
 
 class _LoginForm extends ConsumerWidget {  // ConsumerWidget es propio de riverpod
+
   const _LoginForm();
+
+  void showSnackbar( BuildContext context, String message ){
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) { // Con WidgetRef tenemos acceso a todos los providers de riverpod
 
     final loginForm = ref.watch(loginFormProvider);   // Valor del estado
+    
+    ref.listen(authProvider, (previous, next) { 
+      if( next.errorMessage.isEmpty ) return;
+      showSnackbar( context, next.errorMessage );
+    });
 
     final textStyles = Theme.of(context).textTheme;
 
