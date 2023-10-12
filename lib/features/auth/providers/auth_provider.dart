593f84e0bc11b4fb5e0a6068c7a877ffc0a00404
Jorge Matos <jisapjisap@gmail.com>
2023-10-12 17:39:34 +0200

    shared_preferences

--- lib/features/auth/providers/auth_provider.dart
+++ lib/features/auth/providers/auth_provider.dart
@@ -5,6 +5,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
 import 'package:teslo_shop/features/auth/infraestructure/infrastructure.dart';
+import 'package:teslo_shop/features/shared/infraestructure/services/key_value_storage_service_impl.dart';
+
+import '../../shared/infraestructure/services/key_value_storage_service.dart';
 
 enum AuthStatus { checking, authenticated, notAuthenticated }
 
@@ -36,10 +39,12 @@ class AuthState {
 // Notifier
 class AuthNotifier extends StateNotifier<AuthState> { // Es una observable class que almacena un single immutable [state].
 
-  final AuthRepository authRepository; // Entity (esquema de llamado a la lógica)
+  final AuthRepository authRepository;                  // Entity (esquema de llamado a la lógica)
+  final KeyValueStorageService keyValueStorageService;  // Sistema de almacenamiento del token (instancia)
 
   AuthNotifier({
-    required this.authRepository
+    required this.authRepository,
+    required this.keyValueStorageService
   }): super( AuthState() );
 
   Future<void> loginUser( String email, String password) async {
@@ -68,7 +73,7 @@ class AuthNotifier extends StateNotifier<AuthState> { // Es una observable class
   }
 
   Future<void> logout([ String? errorMessage ]) async {
-    //TODO limpiar token
+    await keyValueStorageService.removeKey('token');
     state = state.copyWith(
       authStatus: AuthStatus.notAuthenticated,
       user: null,
@@ -76,8 +81,8 @@ class AuthNotifier extends StateNotifier<AuthState> { // Es una observable class
     );
   }
 
-  void _setLoggedUser( User user ){
-    //TODO necesito guardar el token en el dispositivo.
+  void _setLoggedUser( User user ) async{
+    await keyValueStorageService.setKeyValue('token', user.token);
     state = state.copyWith(
       user: user,
       authStatus: AuthStatus.authenticated,
@@ -91,6 +96,10 @@ class AuthNotifier extends StateNotifier<AuthState> { // Es una observable class
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) { // Expone el state a la app
   
   final authRepository = AuthRepositoryImpl(); // Llamado a la lógica (datasource_imp). Se usa el datasource que se estableció por defecto
-  
-  return AuthNotifier(authRepository: authRepository); //Instancia (esquema de llamado implementado con la lógica)
+  final keyValueStorageService = KeyValueStorageServiceImpl(); // Sistema de almacenamiento del token 
+
+  return AuthNotifier(
+    authRepository: authRepository,                 // Instancia (esquema de llamado implementado con la lógica)
+    keyValueStorageService: keyValueStorageService  // Instancia del sistema de almacenamiento del token
+  ); 
 });
